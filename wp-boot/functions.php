6ba25dcded5a633fc1f69f995b19b931a6cee97b
Hristo Chakarov <ickata@HristoMBA.local>
2013-11-24 23:20:12 +0200

    Main Menu - admin bar fixes

--- wp-boot/functions.php
+++ wp-boot/functions.php
@@ -8,6 +8,9 @@ function wpboot_enqueue_styles() {
    // any changes to the master bootstrap.css.
    wp_register_style( 'bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.css', array(), '1.0', 'all' );
    wp_enqueue_style( 'bootstrap' );
+   // theme css
+   wp_register_style( 'wpboot', get_stylesheet_uri(), array(), '1.0', 'all' );
+   wp_enqueue_style( 'wpboot' );
 }
 
 // register sidebars
