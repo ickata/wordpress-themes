f6b48732aef5fe8327987668e25245effdca5d90
Hristo Chakarov <ickata@HristoMBA.local>
2013-11-26 19:44:59 +0200

    Make the current menu items active

--- wp-boot/classes/nav-menu-walker.php
+++ wp-boot/classes/nav-menu-walker.php
@@ -11,4 +11,15 @@ class WPBoot_Nav_Walker extends Walker_Nav_Menu {
       // build html
       $output .= '<ul class="dropdown-menu">';
    }
+   
+   // add Bootstrap's "active" class to current menu items
+   function start_el( &$output, $item, $depth, $args ) {
+      if (
+         array_search( 'current_page_item', $item->classes ) > -1 or
+         array_search( 'current_page_parent', $item->classes ) > -1
+      ) {
+         array_push( $item->classes, 'active' );
+      }
+      parent::start_el( $output, $item, $depth, $args );
+   }
 }
