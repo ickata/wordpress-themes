e1fca301bba5a099a0f8f4a065f08ec09d81318f
mpeshev <mario@peshev.net>
2013-11-28 00:07:16 +0200

    Change hook for adding styles (yes, I know it sounds weird)

--- wp-boot/functions.php
+++ wp-boot/functions.php
@@ -3,7 +3,7 @@
 include 'classes/nav-menu-walker.php';
 
 // enqueue resources
-add_action( 'wp_head', 'wpboot_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'wpboot_enqueue_styles' );
 function wpboot_enqueue_styles() {
    // This is the compiled css file from LESS - this means you compile the LESS 
    // file locally and put it in the appropriate directory if you want to make 
